5ce9a09869770f75958d12ee7ae214f51c90bf87
Thomas Auffredou <tauffredou@xebia.fr>
2016-05-29 02:09:17 +0200

    Add provision script

---
+++ provision.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+function checkEnv(){
+    [ -z $(eval echo \${$1}) ] && (
+        echo $1 environnement variable is missing
+        echo
+        echo export $1=$2
+    ) && exit 1;
+}
+
+function usage(){
+    echo "Usage: provision.sh"
+}
+checkEnv AWS_ACCESS_KEY "change me"
+checkEnv AWS_SECRET_KEY "change me"
+checkEnv AWS_REGION "eu-west-1"
+checkEnv AWS_PRIVATE_KEY "/path/to/the/private/key"
+
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+CWD=$(pwd $(dirname $0))
+set -x
+cd $CWD/terraform
+terraform apply --input=false && (
+    node $CWD/tools/terraform2ansible.js $CWD/terraform/terraform.tfstate $CWD/inventory;
+    ansible-playbook -v -i "$CWD/inventory" --private-key="$CWD/terraform/xke-ha-swarm.pem" "$CWD/ansible/site.yml"
+)
