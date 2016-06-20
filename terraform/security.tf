a75ba0f08e3d171d0e34497f385f8b79a56df5d4
Jean-Louis Rigau <jeanlouis.rigau@gmail.com>
2016-06-20 07:22:53 -0700

    Add port 80 to security group

--- terraform/security.tf
+++ terraform/security.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 0
     to_port = 0
     protocol = "icmp"
